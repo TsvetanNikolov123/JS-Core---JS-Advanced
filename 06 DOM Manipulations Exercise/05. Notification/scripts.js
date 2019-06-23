801ff282608451d8b9fa30b5c7dcb2066d02b71a
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-06-23 14:32:12 +0300

    solution  - 05. Notification

---
+++ 06 DOM Manipulations Exercise/05. Notification/scripts.js
@@ -0,0 +1,7 @@
+function notify(message) {
+    let toast = document.getElementById('notification');
+    toast.textContent = message;
+    toast.style.display = 'block';
+
+    setTimeout(() => toast.style.display = 'none', 2000);
+}
